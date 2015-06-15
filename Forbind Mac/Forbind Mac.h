167ebc97b9de1384830a53b1f86d14cf7c46776f
Ulrik Damm <ulrikdamm@me.com>
2015-06-15 13:12:01 +0200

    Mac target

---
+++ Forbind Mac/Forbind Mac.h
@@ -0,0 +1,19 @@
+//
+//  Forbind Mac.h
+//  Forbind Mac
+//
+//  Created by Ulrik Damm on 15/06/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Forbind Mac.
+FOUNDATION_EXPORT double Forbind_MacVersionNumber;
+
+//! Project version string for Forbind Mac.
+FOUNDATION_EXPORT const unsigned char Forbind_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forbind_Mac/PublicHeader.h>
+
+
