bf1371593b3019ade0fdf2b962e0693e966efbae
Ulrik Damm <ulrikdamm@me.com>
2015-06-15 14:11:27 +0200

    Forbind Extensions for Mac

---
+++ ForbindExtensions Mac/ForbindExtensions Mac.h
@@ -0,0 +1,19 @@
+//
+//  ForbindExtensions Mac.h
+//  ForbindExtensions Mac
+//
+//  Created by Ulrik Damm on 15/06/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ForbindExtensions Mac.
+FOUNDATION_EXPORT double ForbindExtensions_MacVersionNumber;
+
+//! Project version string for ForbindExtensions Mac.
+FOUNDATION_EXPORT const unsigned char ForbindExtensions_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForbindExtensions_Mac/PublicHeader.h>
+
+
