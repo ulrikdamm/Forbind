78473cf7cbd57918ef677e0ce90f340e317538cd
Kristian Andersen <hello@kristian.co>
2016-04-26 14:51:10 +0200

    Added tvOS target

---
+++ Forbind tvOS/Forbind tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Forbind tvOS.h
+//  Forbind tvOS
+//
+//  Created by Kristian Andersen on 26/04/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Forbind tvOS.
+FOUNDATION_EXPORT double Forbind_tvOSVersionNumber;
+
+//! Project version string for Forbind tvOS.
+FOUNDATION_EXPORT const unsigned char Forbind_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forbind_tvOS/PublicHeader.h>
+
+
